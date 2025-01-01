0e453c85974358efca294ee97814d8b77232d6ff
Anubhav Singhal <ss.lfsgd@gmail.com>
2025-01-02 03:28:45 +0530

    ready to deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
           <div className="g2"></div>
           <div className="g3"></div>
           <div className="g4"></div>
-          <div className="g5"></div>
+
           <div className="interactive"></div>
         </div>
       </div>
--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -5,7 +5,7 @@ function Landing() {
     <div>
       <div className="w-full lg:w-2/3 mx-auto my-10 flex flex-col justify-center items-center text-md font-bold">
         <p className="mt-10 m-2"> PERP Trading Simplified</p>
-        <p className="text-5xl font-extrabold text-center w-2/3">
+        <p className="text-5xl font-extrabold text-center w-2/3 opacity-90">
           Join The Waitlist For Funded Accounts Today!
         </p>
         <p className="m-2 mb-10">
